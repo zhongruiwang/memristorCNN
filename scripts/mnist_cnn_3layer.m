68698c3c9c006bab67f259bf316ae537ad6ffcca
Zhongrui Wang <zhongrwang@gmail.com>
2019-07-09 13:26:27 -0400

    Add files via upload

--- scripts/mnist_cnn_3layer.m
+++ scripts/mnist_cnn_3layer.m
@@ -1,45 +1,58 @@
-% This script classifies 8x8 MNIST with 5-level CNN implemented on the
-% 1T1R memristor array
+%% CNN for crop and resized MNIST, with Xbar_v7
 
 %% Prepare the dataset
-clear all; close all;
+clear; close all;
 
-% training set resale
-load('mnist_training_8x8_0d2.mat');
-n_samples = 60000;
-xs_trainig = 2*data_0d2(:, 1:n_samples)-0.2;
+% train set
+xs_train = load('mnist_training_8x8_0d2', 'data_0d2');
+xs_train = xs_train.data_0d2;
 
-% trainging set label
-ys_training = zeros(10, n_samples);
-for i = 1:n_samples
-    ys_training(labels(i)+1, i) = 1;
-end
+xs_train = 2 * xs_train - 0.2;
 
-% test set rescale
-load('mnist_test_8x8_0d2.mat');
-n = length(labels);
-xs_test = 2*data_0d2(:, 1:n)-0.2;
+% train labels
+ys_train = load('mnist_train_labels_onehot', 'mnist_train_labels');
+ys_train = ys_train.mnist_train_labels;
 
-% test set label
-ys_test = zeros(10, n);
-for i = 1:n
-    ys_test(labels(i)+1, i) = 1;
-end
+% test set
+xs_test = load('mnist_test_8x8_0d2.mat', 'data_0d2');
+xs_test = xs_test.data_0d2;
+
+xs_test = 2 * xs_test - 0.2;
+
+% test labels
+ys_test = load('mnist_test_labels_onehot', 'mnist_test_labels');
+ys_test = ys_test.mnist_test_labels;
+
+rng(2)
 
 %% Model
 
+% For simu array base
+% f = @(Vt) 200e-6/1.1*Vt;
+% noise = 0.02;
+% update_fun = @(G,V,Vt) -G.*(V ~= 0)+(V > 0).* max(f(Vt),G) +...
+%     noise.*randn(size(G)).*400e-6; 
+% base = multi_array(sim_array1_conv({'random' [123 59] 50e-6 100e-6},...
+%     update_fun,0, Inf));
+
 % For real array base
-load('memristor_1T1R_usable_rows_columns.mat','good_row', 'good_col');
+load('May_25_good_row_col.mat','good_row', 'good_col');
 base = multi_array(real_array2_conv(good_row, good_col));
 
-m = model( xbar_v5( base ) );
+m = model( xbar_v7( base ) );
 
-m.add(unflatten(64, [8 8 1]),                                                             'net_corner', [1 1]);
-m.add(conv2D([3 3 1 15],   'strides', [1 1], 'padding', 'same',   'bias_config', [0,0]),  'net_corner', [62 1], 'dp_rep', [2, 1]);
-m.add(conv2D([2 2 15 4],   'strides', [1 1], 'padding', 'same',   'bias_config', [0,0]),  'net_corner', [9 16], 'dp_rep', [1, 2]);
-m.add(maxpooling2D([2 2],  'strides', [2 2], 'padding', 'valid'),                         'net_corner', [1 1]);
-m.add(flatten(),                                                                          'net_corner', [1 1]);
-m.add(dense(10, 'activation', 'stable_softmax', 'bias_config', [0,0]),                    'net_corner', [1 24], 'dp_rep', [1, 2]);
+% Gate / RESET voltage
+m.backend.Vg_min = 0.5; m.backend.Vg_max = 2.5;
+m.backend.Vg0 = 1.6; m.backend.V_reset = 1.7; m.backend.V_set = 2.5;
+m.backend.ratio_Vg_G = 1.5/100e-6;
+
+% Add layers
+m.add(unflatten(64, [8 8 1]),                                                           'net_corner', [1 1]);
+m.add(conv2D([3 3 1 15],  'strides', [1 1],                         'padding', 'same',  'bias_config', [0,0]),  'net_corner', [88 37], 'dp_rep', [2, 1]);
+m.add(conv2D([2 2 15 4],  'strides', [1 1], 'activation', 'linear', 'padding', 'same',  'bias_config', [0,0]),  'net_corner', [4 52], 'dp_rep', [1, 2]);
+m.add(maxpooling2D([2 2], 'strides', [2 2],                         'padding', 'valid'),                         'net_corner', [1 1]);
+m.add(flatten(),                                                                        'net_corner', [1 1]);
+m.add(dense(10, 'activation', 'stable_softmax', 'bias_config', [0,0]),                  'net_corner', [38 1], 'dp_rep', [1 2 86]);
 
 %% Auxilary
 
@@ -49,13 +62,24 @@ m.v.DEBUG = 1;
 m.summary()
 %draw_weights_layout(base);
 
+no_epoches = 2; % No. of epoches
+ys_test_hardware = cell(no_epoches, 1); % Initialize inference results
+accuracy = NaN(no_epoches, 1); % Initialize accuracy results
+
 %% Fit
 
 m.compile(cross_entropy_softmax_loss(),...
     RMSprop('lr', 0.01) , 'draw', 2, 'save', 1);
-m.fit( xs_trainig, ys_training, 'batch_size', 100, 'epochs', 2);
-    
-%% Test-set
 
-ys_testing_hardware = m.predict(xs_test, 'batch_size', 100);
-accuracy_testset = m.evaluate( ys_testing_hardware, ys_test);
+for epo = 1:no_epoches
+    
+    % Train
+    m.fit(xs_train, ys_train, 'batch_size', 100, 'epochs', 1);
+    
+    % Inference
+    ys_test_hardware{epo} = m.predict(xs_test, 'batch_size', 100);
+    accuracy(epo) = m.evaluate(ys_test_hardware{epo}, ys_test);
+
+end
+
+save('exp_mnist', '-v7.3');
--- scripts/mnistsequence_CLSTM_2layer.m
+++ scripts/mnistsequence_CLSTM_2layer.m
@@ -1,48 +1,75 @@
-% This script classifies the synthetic MNIST-sequence with 3-level ConvLSTM
-% implemented on the 1T1R memristor array
+%% With Xbar_v5
 
 %% Prepare the dataset
-clear all; close all;
+clear; close all;
 
+% train set
 % The input to the conv2DLSTM follows
 % xs = [64 (actually 8x8), n_sample, time_step (here = 3) ]
-load('mnistsequence_train_test.mat');
+load('mnist_conv2DLSTM_training.mat');
 
-% training set rescale
 xs_training = 0.4 * xs_training - 0.2;
-
-% test set rescale
 xs_testing = 0.4 * xs_testing - 0.2;
+
+rng(2)
 
 %% Neural network
 
+% For simu array base
+% f = @(Vt) 200e-6/1.1*Vt;
+% noise = 0.02;
+% update_fun = @(G,V,Vt) -G.*(V ~= 0)+(V > 0).* max(f(Vt),G) +...
+%     noise.*randn(size(G)).*400e-6; 
+% base = multi_array(sim_array1_conv({'random' [128 64] 50e-6 100e-6},...
+%     update_fun,0, Inf));
+
 % For real array base
-load('memristor_1T1R_usable_rows_columns.mat','good_row', 'good_col');
+load('May_22_good_row_col.mat','good_row', 'good_col');
 base = multi_array(real_array2_conv(good_row, good_col));
 
-m = model(xbar_v5( base ));
+m = model( xbar_v5( base ) );
 
+% Gate / RESET voltage
+% m.backend.Vg_min = 0.5; m.backend.Vg_max = 1.8; m.backend.Vg0 = 1.15;
+m.backend.Vg_min = 0.5; m.backend.Vg_max = 2.4;
+m.backend.Vg0 = 1.6; m.backend.V_reset = 1.7; m.backend.V_set = 2.5;
+m.backend.ratio_Vg_G = 1.5/100e-6;
+
+% Add layers
 m.add( unflatten(64, [8 8 1]),                                                                                                  'net_corner', [1 1]);
-m.add( conv2DLSTM([3 3 1 5], [2 2 5 5], 'input_dim', [8 8 1], 'strides_x', [1 1], 'padding_x', 'valid', 'bias_config', [0 0]),  'net_corner', [13 22], 'dp_rep', [2, 1]);
+m.add( conv2DLSTM([3 3 1 5], [2 2 5 5], 'input_dim', [8 8 1], 'strides_x', [1 1], 'padding_x', 'valid', 'bias_config', [0 0]),  'net_corner', [67 1], 'dp_rep', [1, 2]);
 m.add( maxpooling2D([2 2], 'strides', [2 2], 'padding', 'valid'),                                                               'net_corner', [1 1]);
 m.add( flatten(),                                                                                                               'net_corner', [1 1]);
-m.add( dense(6, 'activation', 'stable_softmax', 'bias_config', [0 0]),                                                          'net_corner', [39 10], 'dp_rep', [1, 2]);
+m.add( dense(6, 'activation', 'stable_softmax', 'bias_config', [0 0]),                                                          'net_corner', [31 45], 'dp_rep', [1, 2]);
 
+% CLSTM layer size (29*2 rows 20 cols). 
+% Dense layer size (45*2 rows 6 cols).
 %% Auxilary
 
 m.v.DRAW_PLOT = 1;
 m.v.DEBUG = 1;
 
-m.summary
+m.summary()
 %draw_weights_layout(base);
+
+no_epoches = 2; % No. of epoches
+ys_test_hardware = cell(no_epoches, 1); % Initialize inference results
+accuracy = NaN(no_epoches, 3); % Initialize accuracy results
 
 %% Fit
 
 m.compile(cross_entropy_softmax_loss('recurrent_mode', 'last'),...
     RMSprop('lr', 0.01 ), 'draw', 2, 'save', 1);
-m.fit( xs_training(:,sample_sel,:), ys_training(:,sample_sel,:), 'batch_size', 50, 'epochs', 1);
 
-%% Test-set
+for epo = 1:no_epoches
+    
+    % Train
+    m.fit(xs_training, ys_training, 'batch_size', 50, 'epochs', 1);
+    
+    % Inference
+    ys_test_hardware{epo} = m.predict(xs_testing, 'batch_size', 50);
+    accuracy(epo, 1:3) = m.evaluate(ys_test_hardware{epo}, ys_testing);
 
-ys_testing_hardware = m.predict(xs_testing, 'batch_size', 50);
-accuracy_testset = m.evaluate( ys_testing_hardware, ys_testing);
+end
+
+save('exp_mnistsequence', '-v7.3');
